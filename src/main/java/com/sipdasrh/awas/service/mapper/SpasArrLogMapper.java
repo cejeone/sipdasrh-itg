cc6373dd94f8f49ee0e7df57c2a9cdfb716f352d
Darizki Pratama <dpratama@gmail.com>
2025-08-21 17:58:08 +0700

    (feature) SpasArrLogMapper 2nd level child mapper

--- src/main/java/com/sipdasrh/awas/service/mapper/SpasArrLogMapper.java
+++ src/main/java/com/sipdasrh/awas/service/mapper/SpasArrLogMapper.java
@@ -1,9 +1,11 @@
 package com.sipdasrh.awas.service.mapper;
 
+import com.sipdasrh.awas.domain.Spas;
 import com.sipdasrh.awas.domain.SpasArrInstall;
 import com.sipdasrh.awas.domain.SpasArrLog;
 import com.sipdasrh.awas.service.dto.SpasArrInstallDTO;
 import com.sipdasrh.awas.service.dto.SpasArrLogDTO;
+import com.sipdasrh.awas.service.dto.SpasDTO;
 import org.mapstruct.*;
 
 /**
@@ -18,5 +20,12 @@ public interface SpasArrLogMapper extends EntityMapper<SpasArrLogDTO, SpasArrLog
     @BeanMapping(ignoreByDefault = true)
     @Mapping(target = "id", source = "id")
     @Mapping(target = "namaInstalasi", source = "namaInstalasi")
+    @Mapping(target = "spas", source = "spas", qualifiedByName = "spasId")
     SpasArrInstallDTO toDtoSpasArrInstallId(SpasArrInstall spasArrInstall);
+
+    @Named("spasId")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "namaSpas", source = "namaSpas")
+    SpasDTO toDtoSpasId(Spas spas);
 }
